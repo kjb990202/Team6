1cd6d2b7fd39731f0a0aeb374a8b5314fbf11fb8
zyam1 <zyam8441@gmail.com>
2023-11-11 17:28:17 +0900

    Test:무한스크롤 테스트

---
+++ model/boardTest.js
@@ -0,0 +1,42 @@
+// 임시데이터 생성
+
+exports.boardInfo =() => {
+
+    //mysql연결 select * from board;로 가져왔다고 가정
+    return[
+        {boardID:1, userid:"jiwon", content: "안녕 얘들아 오늘 뭐할거야?"},
+        {boardID:2, userid:"coco",   content: "졸리다2"},
+        {boardID:3, userid:"coco",   content: "졸리다3"},
+        {boardID:4, userid:"coco",   content: "졸리다4"},
+        {boardID:5, userid:"coco",   content: "졸리다5"},
+        {boardID:6, userid:"coco",   content: "졸리다6"},
+        {boardID:7, userid:"coco",   content: "졸리다7"},
+        {boardID:8, userid:"coco",   content: "졸리다8"},
+        {boardID:9, userid:"coco",   content: "졸리다9"},
+        {boardID:10, userid:"coco",   content: "졸리다10"},
+        {boardID:11, userid:"coco",   content: "졸리다11"},
+        {boardID:12, userid:"coco",   content: "졸리다12"},
+        {boardID:13, userid:"coco",   content: "졸리다13"},
+        {boardID:14, userid:"coco",   content: "졸리다14"},
+        {boardID:15, userid:"coco",   content: "졸리다15"},
+        {boardID:16, userid:"반절",   content: "졸리다16"},
+        {boardID:17, userid:"coco",   content: "졸리다17"},
+        {boardID:18, userid:"coco",   content: "졸리다18"},
+        {boardID:19, userid:"coco",   content: "졸리다19"},
+        {boardID:20, userid:"coco",   content: "졸리다20"},
+        {boardID:21, userid:"coco",   content: "졸리다21"},
+        {boardID:22, userid:"coco",   content: "졸리다22"},
+        {boardID:23, userid:"coco",   content: "졸리다23"},
+        {boardID:24, userid:"coco",   content: "졸리다24"},
+        {boardID:25, userid:"래래",   content: "졸리다25"},
+        {boardID:26, userid:"coco",   content: "졸리다26"},
+        {boardID:27, userid:"coco",   content: "졸리다27"},
+        {boardID:28, userid:"coco",   content: "졸리다28"},
+        {boardID:29, userid:"coco",   content: "졸리다29"},
+        {boardID:30, userid:"coco",   content: "졸리다30"},
+        
+
+
+
+];
+}
