8546e12588cbf2611b4ab164bc7f422cb6081443
nayounghye <yhna.dev@gmail.com>
2023-11-13 15:08:04 +0900

    Feat: 게시판 작성 기능 구현, 코드 일부 수정(DB구축 후 테스트 필요)

--- controller/index.js
+++ controller/index.js
@@ -22,24 +22,3 @@ exports.boardMain = (req, res) => {
 exports.boardEdit = (req, res) => {
   res.render("board/boardEdit");
 };
-
-// 게시판 작성 화면 -> 게시글 등록
-const { Board } = require("../model"); // Board 모델 가져오기
-exports.boardSubmit = async (req, res) => {
-  try {
-    const { category_id, title, content, nickname } = req.body;
-
-    // Board 모델을 사용하여 데이터베이스에 저장
-    await Board.create({
-      category_id,
-      title,
-      content,
-      nickname,
-    });
-
-    res.status(200).json({ message: "게시글이 성공적으로 등록되었습니다." });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "서버 오류 발생" });
-  }
-};
