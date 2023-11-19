2a447fe16af090dda9043911bd59d4be5547efbc
“seobon” <seobon1201@naver.com>
2023-11-19 21:15:38 +0900

    Fix: 브라우저 화면 구성 변경, 그에 맞게 기능 오류 수정

--- model/Map_Database.js
+++ model/Map_Database.js
@@ -1,5 +1,3 @@
-// const {User, Map_Information} = require('./index');
-
 function Map_Database(Sequelize, DataTypes) {
     // sequelize 객체의 define이라는 메소드를 이용해서 모델(테이블)을 정의한다.
     return Sequelize.define(
--- model/Map_Information.js
+++ model/Map_Information.js
@@ -1,5 +1,3 @@
-const Map_Database = require('./Map_Database');
-
 function Map_Information(Sequelize, DataTypes) {
     // sequelize 객체의 define이라는 메소드를 이용해서 모델(테이블)을 정의한다.
     return Sequelize.define(
@@ -26,8 +24,5 @@ function Map_Information(Sequelize, DataTypes) {
             timestamps: false,
         });
 };
-
-// Map_Information.hasMany(Map_Database, {foreignKey: 'storeID'});
-// Map_Database.belongsTo(Map_Information, { foreignKey: 'storeID' });
 
 module.exports = Map_Information;
