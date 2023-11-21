b45fe3fa5fd24e046bee5838117ab2c7892984c0
kjb990202 <kjb990202@gmail.com>
2023-11-21 18:07:02 +0900

    Feat: 작성시간 UTC로 수정해야함

--- model/Board.js
+++ model/Board.js
@@ -3,7 +3,7 @@ const Map_Database = require('./Map_Database');
 function Board(Sequelize, DataTypes) {
     // sequelize 객체의 define이라는 메소드를 이용해서 모델(테이블)을 정의한다.
     return Sequelize.define(
-        'Board', // 테이블 이름
+        'board', // 테이블 이름
         {
 // // 게시글 작성
 // module.exports = (sequelize, DataTypes) => {
@@ -23,12 +23,12 @@ function Board(Sequelize, DataTypes) {
         allowNull: true,
       },
       category: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
+        type: DataTypes.TEXT,
+        allowNull: true,
       },
       content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+        type: DataTypes.STRING(20),
+        allowNull: true,
       },
       views: {
         type: DataTypes.INTEGER,
@@ -36,20 +36,20 @@ function Board(Sequelize, DataTypes) {
       },
       createBoard: {
         type: DataTypes.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        allowNull: true,
       },
       modifiedBoard: {
         type: DataTypes.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        allowNull: true,
       },
       id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
       },
     },
     {
       // 모델 옵션
-      tableName: "Board",
+      tableName: "board",
       frezzeTableName: true,
       timestamps: false, // createdAt과 updatedAt 컬럼을 사용하지 않음
       freezeTableName: true,
